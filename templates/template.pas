b04b08d3c420290e07c90fe0b57c6f0a11d5c124
Pavlo Golub <pavlo.golub@gmail.com>
2020-10-01 20:12:30 +0200

    make IP simple string without check

--- templates/template.pas
+++ templates/template.pas
@@ -13,6 +13,7 @@ type
   TVIPManager = class(TComponent)
   private
     FVirtualIP: TIPAddress;
+    FIP: string;
     FMask: string;
     FInterfaceName: string;
     FKey: string;
@@ -23,7 +24,7 @@ type
   public
     constructor Create(AOwner: TComponent); override;
   published
-    property IP: string read GetVirtualIP write SetVirtualIP;
+    property IP: string read FIP write FIP;
     property Mask: string read FMask write FMask;
     property InterfaceName: string read FInterfaceName write FInterfaceName;
     property Key: string read FKey write FKey;
